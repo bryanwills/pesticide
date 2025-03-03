57010a8f0bb12202d8d4a921a3834dcc766c981f
Bryan Wills <bryanwi09@gmail.com>
2025-03-03 16:46:27 -0500

    updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,137 +1,101 @@
-// Load plugins
+const gulp = require("gulp");
+const { parallel, series, watch } = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const size = require("gulp-size");
+const shell = require("gulp-shell");
 
-var gulp          = require('gulp'),
-    gutil         = require('gulp-util'),
-    watch         = require('gulp-watch'),
-    prefix        = require('gulp-autoprefixer'),
-    minifyCSS     = require('gulp-minify-css'),
-    sass          = require('gulp-sass'),
-    less          = require('gulp-less'),
-    stylus        = require('gulp-stylus'),
-    csslint       = require('gulp-csslint'),
-    concat        = require('gulp-concat'),
-    uglify        = require('gulp-uglify'),
-    shell         = require('gulp-shell'),
-    rename        = require('gulp-rename'),
-    size          = require('gulp-size'),
-    browserSync   = require('browser-sync'),
-    browserReload = browserSync.reload,
-    preProcessor  = 'sass';
+// Configuration
+const config = {
+  preprocessor: "sass",
+  paths: {
+    css: {
+      src: "./css/pesticide.css",
+      dest: "./css/",
+    },
+    js: {
+      src: "./js/pesticide.js",
+      dest: "./js/",
+    },
+    styles: {
+      sass: "./sass/**/*.scss",
+    },
+    html: "./*.html",
+  },
+};
 
-// Task to generate all source files with proper color table
+// Generate color table - FIXED
+const generate = () => {
+  return gulp
+    .src("package.json") // Use any existing file as dummy input
+    .pipe(shell(["node ./generate_color_table.js"]));
+};
 
-gulp.task('generate', shell.task([
-  'node ./generate_color_table.js'
-]));
+// CSS processing
+const processCSS = () => {
+  return gulp
+    .src(config.paths.css.src)
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(size({ gzip: true, showFiles: true, title: "Minified CSS" }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest(config.paths.css.dest))
+    .pipe(browserSync.stream());
+};
 
-// Task to minify all css files in the css directory
-
-gulp.task('minify-css', function(){
-  gulp.src('./css/pesticide.css')
-    .pipe(minifyCSS())
-    .pipe(size({gzip: true, showFiles: true, title:'minified css'}))
-    .pipe(rename('pesticide.min.css'))
-    .pipe(gulp.dest('./css/'));
-});
-
-// Task to compile the bookmarklet version of Pesticide
-
-gulp.task('minify-js', function() {
-  gulp.src('./js/pesticide.js')
+// JavaScript processing
+const processJS = () => {
+  return gulp
+    .src(config.paths.js.src)
     .pipe(uglify())
-    .pipe(concat('pesticide.min.js'))
-    .pipe(gulp.dest('./js/'));
-});
+    .pipe(concat("pesticide.min.js"))
+    .pipe(gulp.dest(config.paths.js.dest))
+    .pipe(browserSync.stream());
+};
 
-// Use csslint without box-sizing or compatible vendor prefixes (these
-// don't seem to be kept up to date on what to yell about)
+// Sass compilation
+const compileSass = () => {
+  return gulp
+    .src(config.paths.styles.sass)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(size({ title: "SASS Compiled" }))
+    .pipe(gulp.dest(config.paths.css.dest));
+};
 
-gulp.task('csslint', function(){
-  gulp.src('./css/*.css')
-  .pipe(csslint({
-    'compatible-vendor-prefixes': false,
-    'box-sizing': false,
-    'important': false,
-    'known-properties': false
-  }))
-  .pipe(csslint.reporter());
-});
+// BrowserSync server
+const serve = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+    notify: false,
+  });
+  done();
+};
 
-// Initialize browser-sync which starts a static server also allows for
-// browsers to reload on filesave
-gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        server: {
-            baseDir: "./"
-        }
-    });
-});
+// Watch files
+const watchFiles = () => {
+  watch(config.paths.styles.sass, series(compileSass, processCSS));
+  watch(config.paths.js.src, processJS);
+  watch(config.paths.html).on("change", browserSync.reload);
+};
 
-// Function to call for reloading browsers
-gulp.task('bs-reload', function () {
-    browserSync.reload();
-});
+// Export tasks
+exports.generate = generate;
+exports.css = processCSS;
+exports.js = processJS;
+exports.sass = compileSass;
+exports.serve = serve;
+exports.watch = watchFiles;
 
-
-// Task that compiles scss files down to good old css
-
-gulp.task('sass', function(){
-  gulp.src('./sass/pesticide.scss')
-  .pipe(watch(function(files) {
-    return files.pipe(sass())
-    .pipe(prefix())
-    .pipe(size({gzip: false, showFiles: true, title:'unminified css'}))
-    .pipe(size({gzip: true, showFiles: true, title:'unminified css'}))
-    .pipe(gulp.dest('./css/'))
-  }));
-});
-
-// Task that compiles less files down to good old css
-
-gulp.task('less', function () {
-  gulp.src('./less/pesticide.less')
-  .pipe(watch(function(files) {
-    return files.pipe(less())
-    .pipe(prefix())
-    .pipe(size({gzip: false, showFiles: true, title:'unminified css'}))
-    .pipe(size({gzip: true, showFiles: true, title:'unminified css'}))
-    .pipe(gulp.dest('./css/'))
-  }));
-});
-
-// Task that compiles stylus files down to good old css
-
-gulp.task('stylus', function () {
-  gulp.src('./stylus/*.styl')
-  .pipe(watch(function(files) {
-    return files.pipe(stylus())
-    .pipe(prefix())
-    .pipe(size({gzip: false, showFiles: true, title:'unminified css'}))
-    .pipe(size({gzip: true, showFiles: true, title:'unminified css'}))
-    .pipe(gulp.dest('./css/'))
-  }));
-});
-
-
-/*
-   DEFAULT TASK
-
- • Process sass and lints outputted css
- • Outputted css is run through autoprefixer
- • Runs jshint on all js files in ./js/
- • Sends updates to any files in directory to browser for
- automatic reloading
-
-*/
-
-gulp.task('default', function(){
-  var preProcessorExtensions = {
-    'sass': '.scss',
-    'less': '.less',
-    'stylus': '.stylus'
-  };
-  gulp.run(preProcessor, 'csslint');
-    gulp.watch(['*.html', './' + preProcessor + '/*' + preProcessorExtensions[preProcessor]], function(event) {
-      gulp.run(preProcessor, 'csslint', 'bs-reload');
-    });
-});
+// Default task (removed generate from main flow)
+exports.default = series(
+  parallel(compileSass, processCSS, processJS),
+  serve,
+  watchFiles,
+);
